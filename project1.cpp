499d4213035313e08611c3ca45116534eb269f62
Pashtet32 <pavelt0309@gmail.com>
2015-09-21 23:33:23 +0200

    Test1

--- project1.cpp
+++ project1.cpp
@@ -1,9 +1,10 @@
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-double sinus (double x, double epsilon)
+double sinus(double x, double epsilon)
 {
 	double sum = 0;
 	double element = x;
@@ -11,12 +12,12 @@ double sinus (double x, double epsilon)
 	while (element > epsilon || 0.0 - element > epsilon)
 	{
 		sum += element;
-		n ++;
+		n++;
 		if (n % 2 == 0)
 			element = 1;
 		else
 			element = -1;
-		for (int i = 1; i <= 2 * n + 1; i ++)
+		for (int i = 1; i <= 2 * n + 1; i++)
 		{
 			element *= x;
 			element /= i;
@@ -24,13 +25,38 @@ double sinus (double x, double epsilon)
 	}
 	return sum;
 }
-
+void testing(double x, double epsilon)
+{
+	double true_sin, test_sin;
+	true_sin = sin(x);
+	test_sin = sinus(x, epsilon);
+	if (abs(true_sin - test_sin) - epsilon <= 0.0)
+	{
+		cout << "OK" << endl;
+	}
+	else
+		cout << "FAIL" << endl;
+}
 int main()
 {
+	/*
 	double x, epsilon;
 	cin >> x >> epsilon;
-	double sin = sinus (x, epsilon);
+	double sin = sinus(x, epsilon);
 	cout << sin << endl;
+	*/
+
+	testing(1, 1);
+	testing(2, 0.1);
+	testing(3, 0.01);
+	testing(4, 0.001);
+	testing(5, 0.0001);
+	testing(6, 0.00001);
+	testing(7, 2);
+	testing(3.14, 0.5);
+	testing(6.2831, 0.0001);
+	testing(100, 1);
+
 	getchar();
 	getchar();
 	return 0;
